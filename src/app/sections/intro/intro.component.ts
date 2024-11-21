769da50fd5fa53f35b41d4dbd742f5c638f22993
liamoctavio <ocmolina47@gmail.com>
2024-11-21 04:02:15 -0300

    Actualizacion informacion y detalles

--- src/app/sections/intro/intro.component.ts
+++ src/app/sections/intro/intro.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.css'
 })
-export class IntroComponent {
+export class IntroComponent implements OnInit {
+
+  ngOnInit() {
+    const options = {
+      strings: ['Desarrollador Web', 'Ingeniero de Software', 'Freelancer'], // Palabras a mostrar
+      typeSpeed: 100, // Velocidad de escritura
+      backSpeed: 100, // Velocidad de borrado
+      loop: true // Repetir el ciclo
+    }; 
+
+    new Typed('#typed-text', options); // Conecta Typed.js al span con ID 'typed-text'
+      
+  }
 
 }
